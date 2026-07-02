ed8d64f0f144c2e5af94c0b66856e639865c8451
LeeWeed091205 <legiahuy912005@gmail.com>
2026-07-02 12:46:03 +0700

    feat: db config func to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import app from './server.js';
 import config from './config/env.js';
+import connectDatabase from './config/database.js';
 
 const startServer = async () => {
   try {
+    await connectDatabase();
     app.listen(config.port, () => {
       console.log(`✓ Server running on http://localhost:${config.port}`);
     });
